9229a03892faf335cd52b354f1c8ba9886ab716a
charinta <charintakantidewi@gmail.com>
2023-09-18 21:41:46 +0700

    tugas2

--- lib/ui/form_data.dart
+++ lib/ui/form_data.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:tugas_2/ui/tampil_data.dart';
 
 class FormData extends StatefulWidget {
   const FormData({Key? key}) : super(key: key);
@@ -9,12 +10,59 @@ class FormData extends StatefulWidget {
 }
 
 class _FormDataState extends State<FormData> {
+  final namaController = TextEditingController();
+  final nimController = TextEditingController();
+  final tahunLahirController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Input Data"),
+        title: const Text("Input Data"),
       ),
+      body: Container(
+          margin: EdgeInsets.all(10),
+          child: Column(
+            children: [
+              _textboxNama(),
+              _textboxNim(),
+              _textboxTahunLahir(),
+              _tombolSimpan()
+            ],
+          )),
     );
+  }
+
+  _textboxNama() {
+    return TextField(
+      decoration: const InputDecoration(labelText: "Nama"),
+      controller: namaController,
+    );
+  }
+
+  _textboxNim() {
+    return TextField(
+      decoration: const InputDecoration(labelText: "Nim"),
+      controller: nimController,
+    );
+  }
+
+  _textboxTahunLahir() {
+    return TextField(
+      decoration: const InputDecoration(labelText: "Tahun Lahir"),
+      controller: tahunLahirController,
+    );
+  }
+
+  _tombolSimpan() {
+    return ElevatedButton(
+        onPressed: () {
+          String nama = namaController.text;
+          String nim = nimController.text;
+          int tahunLahir = int.parse(tahunLahirController.text);
+          Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) =>
+                  TampilData(nama: nama, nim: nim, tahunLahir: tahunLahir)));
+        },
+        child: const Text('Simpan'));
   }
 }
